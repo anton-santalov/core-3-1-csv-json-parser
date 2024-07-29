30fba4fa6f7c0a3dfae5f8be5bb9e132c9fcf58a
Anton Santalov <anton.santalov1995@gmail.com>
2024-07-29 21:59:57 +0400

    Solution for task 2 added

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,21 +7,64 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
-        String fileName = "data.csv";
+        String csvFileName = "data.csv";
+        String xmlFileName = "data.xml";
 
-        List<Employee> list = parseCSV(columnMapping, fileName);
-        String json = listToJson(list);
+        // CSV to JSON
+        List<Employee> csvList = parseCSV(columnMapping, csvFileName);
+        String csvJson = listToJson(csvList);
+        writeString(csvJson, "data.json");
 
-        writeString(json, "data.json");
+        // XML to JSON
+        List<Employee> xmlList = parseXML(xmlFileName);
+        String xmlJson = listToJson(xmlList);
+        writeString(xmlJson, "data2.json");
+    }
+
+    private static List<Employee> parseXML(String fileName) {
+        List<Employee> employeeList = new ArrayList<>();
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(fileName);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    employeeList.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return employeeList;
     }
 
     private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
